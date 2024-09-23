9d99f8664029affababc84a513bd85d2b3857408
new <new@Pranavs-Mac-mini.local>
2024-09-24 03:17:28 +0530

    commit

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBm4doekL9W3Jr9jpblTacyzBm35aJMC4g",
-    authDomain: "nitropay-f1182.firebaseapp.com",
-    projectId: "nitropay-f1182",
-    storageBucket: "nitropay-f1182.appspot.com",
-    messagingSenderId: "549275767693",
-    appId: "1:549275767693:web:4b344eb7432c728e8363c4",
-    measurementId: "G-ZNW4K9JETN"
+  apiKey: "AIzaSyC3EPopzoobbwyYlKkq71lQmuouYPjeLVg",
+  authDomain: "berabucks-71ead.firebaseapp.com",
+  projectId: "berabucks-71ead",
+  storageBucket: "berabucks-71ead.appspot.com",
+  messagingSenderId: "626628372457",
+  appId: "1:626628372457:web:4c0cfbecdc7423aede8d78",
+  measurementId: "G-60CTPT9GY0"
+
+  // apiKey: "AIzaSyCL4u2tDZYNaU_oh82l9z7pFDk5v4i23R4",
+  // authDomain: "amongthenations-195b6.firebaseapp.com",
+  // projectId: "amongthenations-195b6",
+  // storageBucket: "amongthenations-195b6.appspot.com",
+  // messagingSenderId: "897474413583",
+  // appId: "1:897474413583:web:b19c36939f4efe77cdcf7a",
+  // measurementId: "G-GNK14T42H3"
 };
 
 
--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const userCollection = collection(db, "user");
 const app=express()
 app.use(cors());
 
-const BOT_TOKEN = '7118396575:AAFvahtATlJdTT6AvDFdCduF9GePNJ52aX4';
+// const BOT_TOKEN = '7118396575:AAFvahtATlJdTT6AvDFdCduF9GePNJ52aX4';
+// const BOT_TOKEN='7096079855:AAGj6EnVPqK3PuvkJEgrjYzkzXx6Coc4ZEQ' ;
+
+const BOT_TOKEN='7366624473:AAETLvShHK3UAg3nh6PTC3SLjuNn-Uc_lwI'
 
 let bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
 let mapping={}
+
 
 
 bot.onText(/\/start/,async (msg) =>{
@@ -32,6 +36,7 @@ bot.onText(/\/start/,async (msg) =>{
        
            
             let otp=Math.floor(1000 + Math.random() * 9000)
+            let referralCode=msg.chat.username+`${Math.floor(1000 + Math.random() * 9000)}`
             const data = await getDocs(userCollection);
      
             let db=await data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
@@ -51,11 +56,11 @@ bot.onText(/\/start/,async (msg) =>{
             if(flag==0)
                 {
                     try{
-                        await addDoc(userCollection,{username:msg.chat.username,chatId:chatId,otp:otp});
+                        await addDoc(userCollection,{username:msg.chat.username,chatId:chatId,otp:otp,coins:0,referralCode:referralCode,highscore:0,friends:[]});
                         bot.sendMessage(chatId, `If you are not a robot 🤖\n\nEnter your username (${msg.chat.username}) and the code in the Mini App ${otp}`);
                     }
                     catch(error){
-                        bot.sendMessage(chatId, `Username not found`)
+                        bot.sendMessage(chatId, `Username not found. Set your username from profile settings`)
                     }
                    
                 }
