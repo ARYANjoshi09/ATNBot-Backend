9ca2eaf18b8a93133139b84e60b68150070e570a
Ayush <letsmailayushtiwari@gmail.com>
2024-09-30 19:24:40 +0530

    Added git ignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import TelegramBot from 'node-telegram-bot-api';
-import express from 'express'
-import cors from 'cors';
+import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+import cors from "cors";
 import { db } from "./firebase-config.js";
 import {
   collection,
@@ -12,106 +12,86 @@ import {
 } from "firebase/firestore";
 const userCollection = collection(db, "user");
 
-const app=express()
+const app = express();
 app.use(cors());
 
 // const BOT_TOKEN = '7118396575:AAFvahtATlJdTT6AvDFdCduF9GePNJ52aX4';
 // const BOT_TOKEN='7096079855:AAGj6EnVPqK3PuvkJEgrjYzkzXx6Coc4ZEQ' ;
 
-const BOT_TOKEN='7366624473:AAETLvShHK3UAg3nh6PTC3SLjuNn-Uc_lwI'
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 let bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-let mapping={}
+let mapping = {};
 
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
 
+  console.log(msg.chat.username);
 
-bot.onText(/\/start/,async (msg) =>{
+  let otp = Math.floor(1000 + Math.random() * 9000);
+  let referralCode =
+    msg.chat.username + `${Math.floor(1000 + Math.random() * 9000)}`;
+  const data = await getDocs(userCollection);
 
-    const chatId = msg.chat.id;
-  
-    console.log(msg.chat.username)
- 
+  let db = await data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  let flag = 0;
 
-       
-           
-            let otp=Math.floor(1000 + Math.random() * 9000)
-            let referralCode=msg.chat.username+`${Math.floor(1000 + Math.random() * 9000)}`
-            const data = await getDocs(userCollection);
-     
-            let db=await data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-            let flag=0;
-       
-            for(let i=0;i<db.length;i++)
-               {
-                   const username=msg.chat.username;
-                 
-                   if(db[i].username==username)
-                       {
-                          flag=1;
-                          break;
-                       }
-                   
-                   }
-            if(flag==0)
-                {
-                    try{
-                        await addDoc(userCollection,{username:msg.chat.username,chatId:chatId,otp:otp,coins:0,referralCode:referralCode,highscore:0,friends:[]});
-                        bot.sendMessage(chatId, `If you are not a robot 🤖\n\nEnter your username (${msg.chat.username}) and the code in the Mini App ${otp}`);
-                    }
-                    catch(error){
-                        bot.sendMessage(chatId, `Username not found. Set your username from profile settings`)
-                    }
-                   
-                }
-            
-        
-    
-  
-  })
-app.get('/getauth', async(req,res)=>{
+  for (let i = 0; i < db.length; i++) {
+    const username = msg.chat.username;
 
-    
-    try{
-
-        const data = await getDocs(userCollection);
-     
-        let dbdata=await data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-   
-        for(let i=0;i<dbdata.length;i++)
-           {
-               const username=req.query.username;
-               const otp=req.query.otp
-               if(dbdata[i].username==username)
-                   {
-                       dbdata[i].otp==otp
-                       {
-                           res.send('success')
-                       }
-                   }
-               else{
-                   res.send('faliure')
-               }
-               }
-               res.send('faliure')
-           }
-    
-    catch{
-       console.log('Internal sever error')
+    if (db[i].username == username) {
+      flag = 1;
+      break;
     }
-}
-    
-   
-    )
-       
-    
+  }
+  if (flag == 0) {
+    try {
+      await addDoc(userCollection, {
+        username: msg.chat.username,
+        chatId: chatId,
+        otp: otp,
+        coins: 0,
+        referralCode: referralCode,
+        highscore: 0,
+        friends: [],
+      });
+      bot.sendMessage(
+        chatId,
+        `If you are not a robot 🤖\n\nEnter your username (${msg.chat.username}) and the code in the Mini App ${otp}`
+      );
+    } catch (error) {
+      bot.sendMessage(
+        chatId,
+        `Username not found. Set your username from profile settings`
+      );
+    }
+  }
+});
+app.get("/getauth", async (req, res) => {
+  try {
+    const data = await getDocs(userCollection);
 
+    let dbdata = await data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
-    
+    for (let i = 0; i < dbdata.length; i++) {
+      const username = req.query.username;
+      const otp = req.query.otp;
+      if (dbdata[i].username == username) {
+        dbdata[i].otp == otp;
+        {
+          res.send("success");
+        }
+      } else {
+        res.send("faliure");
+      }
+    }
+    res.send("faliure");
+  } catch {
+    console.log("Internal sever error");
+  }
+});
 
- 
-
-
-app.listen(8000,()=>{
-    console.log('App listeing to port 8000')
-})
+app.listen(8000, () => {
+  console.log("App listeing to port 8000");
+});
